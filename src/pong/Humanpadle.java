c8e98133395c813e52db037a6788a7a99bb30b1b
Edsamwel Kayombo <Gaidon44>
2020-08-18 08:22:14 +0300

    created core functions of the game

---
+++ src/pong/Humanpadle.java
@@ -0,0 +1,36 @@
+ package pong;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Humanpadle implements paddle {
+	
+	public Humanpadle(int player) {
+	
+	}
+
+	public void draw(Graphics g) {
+	
+		
+	}
+
+	public void move() {
+		
+	}
+	
+	public void setUpAccel(boolean input) {
+
+		
+	}
+	
+	public void setDownAccel(boolean input) {
+		
+	}
+	
+	public int getY() {
+		
+	
+		
+	}
+
+}
---
+++ src/pong/Tennis.java
@@ -0,0 +1,41 @@
+package pong;
+ 
+import java.applet.Applet;	
+import java.awt.Graphics;
+
+
+public class Tennis  extends Applet{
+
+	public void init() {
+		
+		  
+	}
+	
+	public void paint(){
+		
+	}
+	
+	public void update() {
+
+	}
+	
+	public void run(){
+	
+		
+	}
+
+	public void keyPressed() {
+	
+		
+	}
+
+	public void keyReleased() {
+	
+		
+	}
+	public void keyTyped() {
+		
+		
+	}
+
+}
